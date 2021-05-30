8546bb7b0ae6c206c955f53d311d7fb8d120d409
Abriham Mekonnen <>
2021-05-30 01:28:54 +0000

    added a subreddit

--- main.py
+++ main.py
@@ -2,20 +2,16 @@ import os
 import praw
 
 
-
-
-
-
 reddit = praw.Reddit(
   client_id = os.environ['client_id'],
   client_secret = os.environ['client_secret'],
   
   username = os.environ['username'],
   password = os.environ['password'],
-  user_agent = "<ReplyCommentBot1.0>"
+  user_agent = "<ReplyCommentBot1.0"
 )
 
 
 subreddit = reddit.subreddit("ProgrammerHumor")
-for post in subreddit.hot(limit = 10):
-  print(post.title)
+for comment in subreddit.stream.comments(skip_existing=True):
+  print(comment.body)
